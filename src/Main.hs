1fd0a8e4f7c1a0fecade953ed4be3fdfe61728fb
Richard Fung <minesasecret@gmail.com>
2017-01-06 21:40:42 -0800

    Init again

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Applicative
+import           Snap.Core
+import           Snap.Util.FileServe
+import           Snap.Http.Server
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = quickHttpServe site
+
+site :: Snap ()
+site =
+    ifTop (writeBS "hello world") <|>
+    route [ ("foo", writeBS "bar")
+          ]
